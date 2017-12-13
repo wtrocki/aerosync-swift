8dfdc9c4a3eff195fb3ba297f0faf1bbc433341a
Wojciech Trocki <wtrocki@redhat.com>
2017-12-13 15:16:41 +0000

    Initial Commit

---
+++ aerosync-swift/aerosync_swift.h
@@ -0,0 +1,19 @@
+//
+//  aerosync_swift.h
+//  aerosync-swift
+//
+//  Created by Wojciech Trocki on 12/13/17.
+//  Copyright © 2017 aerogear. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for aerosync_swift.
+FOUNDATION_EXPORT double aerosync_swiftVersionNumber;
+
+//! Project version string for aerosync_swift.
+FOUNDATION_EXPORT const unsigned char aerosync_swiftVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <aerosync_swift/PublicHeader.h>
+
+
